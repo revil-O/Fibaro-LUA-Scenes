b438a30ba48cc2d4c0bb36ee50ea24d6d565e8c2
revil-O <os.germany@icloud.com>
2016-08-18 12:38:42 +0200

    Create wallplug_auto_off.lua
    @de - LUA-Szene um Fibaro-Wallplugs auszuschalten, wenn der aktuelle Verbrauch über einen definierten Zeitraum eine Mindestschwelle unterschritten hat - hier:     40 Watt über einen Zeitraum von 4 Minuten
    
    @en - lua-scene to turn off Fibaro wallplugs automatically, if the energy consumption is lower than a specified value during a specfied time --> in this case : 40 watts during 4 minutes

---
+++ wallplug_auto_off.lua
@@ -0,0 +1,44 @@
+-- @file      wallplug_auto_off.lua
+-- @author    oliver schmidt
+-- @licence   creative commons
+-- @date      2016-08-18
+
+--[[
+%% properties
+%% autostart
+%% globals
+--]]
+local sourceTrigger = fibaro:getSourceTrigger();
+local version = '1.0';
+local sleep_value = 60000;          -- SleepValue for while-loop
+local sleep_counter = 0;            -- initialize sleep_counter
+local low_power = 40;               -- low-power-value  --> turn off device when powerconsumption is lower than specified value (watts)
+local wall_plug = 57;               -- fibaro-ID of the desired wallplug 
+local set_off_time = 240000;        -- 240 seconds -- 4 minutes
+
+fibaro:debug('Automatische WallplugAbschaltung bei EnergyLevel unter 40 Watt nach 3 Minuten (Ver.' .. version .. ')');
+
+while true do
+     fibaro:debug('SleepCounter: ' ..sleep_counter);
+     local wp_power = tonumber(fibaro:getValue(wall_plug,"power"));
+	 --fibaro:debug(wp_power);
+     if(wp_power > 0) 
+       then fibaro:debug('WallPlug ist eingeschaltet..');
+    		fibaro:debug('Aktueller Verbrauch: '.. wp_power ..' Watt.');
+       else fibaro:debug('Wallplug ist ausgeschaltet');
+     end;
+     if (wp_power < low_power and wp_power > 0) then
+     	sleep_counter = sleep_counter + sleep_value;
+     else 
+    	-- Zurücksetzen des SleepCounter wenn die angeschlossenen Geräte wieder
+    	-- innnerhalb der eingestellten set_off_time eingeschaltet und genutzt werden 
+        sleep_counter = 0;
+     end;
+     if(sleep_counter == set_off_time and sleep_counter > 0)
+    	then fibaro:debug('WallPlug wird nach ' .. set_off_time/1000/60 .. ' Minuten unter ' .. low_power .. ' \n Watt Verbrauch abgeschaltet.' );
+     		 fibaro:call(wall_plug,"turnOff");
+    		 sleep_counter = 0;
+     end;
+     fibaro:sleep(sleep_value)
+     
+end
